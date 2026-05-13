76094cd5c6564131659759e1823fb71719562dda
ZTRCOMPANY1 <ztrcompany1@gmail.com>
2026-05-13 16:22:48 -0300

    auto update

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 const API_DASHBOARD = "https://servidor-dashboard-race-low-poly.onrender.com";
-const API_ADMIN = "https://servidor-site-race-low-poly-atualizacoes.onrender.com";
+const API_ADMIN = "https://servidor-site-race-low-poly-atualizacoes-y93n.onrender.com";
